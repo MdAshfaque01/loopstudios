d32ad079fc7b19b665df63a5901a0fcfd38adf83
Md Ashfaque <103313835+MdAshfaque01@users.noreply.github.com>
2022-08-19 18:56:38 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      fontFamily:{
+        alata:["Alata"],
+      },
+      letterSpacing : {
+        widest:'0.3em'
+      }
+      
+    },
+  },
+  plugins: [],
+}
